b4bd705378e3a034cf78f31e4a10039857e59bf5
Muhammad <muxammad_205@mail.ru>
2024-01-05 14:33:00 +0500

    last view

--- src/@core/components/pages/main/actions.tsx
+++ src/@core/components/pages/main/actions.tsx
@@ -2,7 +2,7 @@ import { IGlobalDataType } from '@/@core/utils/type'
 
 export const getData = async (locale: unknown) => {
   try {
-    const res = await fetch(`http://192.168.61.169:2001/api/v1/getAllpageData/${locale}`)
+    const res = await fetch(`https://apilanding.ccenter.uz/api/v1/getAllpageData/${locale}`)
     const data: IGlobalDataType = await res.json()
 
     return data
